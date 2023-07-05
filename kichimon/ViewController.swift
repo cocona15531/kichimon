0458f356de2170e77c5f71c30aacbcc5d4db1ee7
issei.ueda <ron.ron633662@gmail.com>
2023-07-05 18:36:55 +0900

    Initial Commit

---
+++ kichimon/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  kichimon
+//
+//  Created by Issei Ueda on 2023/07/05.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
